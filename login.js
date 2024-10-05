030a397ad9b8bd4dd6b16662f0bf0f0fc643e814
Göndöcs Gergő <44317190+Saynland@users.noreply.github.com>
2024-10-05 10:25:11 +0200

    órai bejelentkezős felület

---
+++ login.js
@@ -0,0 +1,47 @@
+let userek = [
+    {username:'zita', password:'kiscica'},
+    {username:'béla', password:'lapát'},
+    {username:'anna', password:'kanna'},
+
+    
+]
+
+function belepes() {
+    let felh = document.getElementById('felhasznalo').value
+    let jelsz = document.getElementById('jelszo').value
+
+    if (!(felh && jelsz)){
+        alert("Nem adtál meg adatokat!")
+        return
+    }
+
+/* 
+   let idx =-1
+    for(let i = 0; i<userek.length; i++){
+        if(userek[i]['username']== felh){
+            idx=i
+        }
+    } */
+
+    let idx = userek.findIndex((user)=>user.username == felh && user.password == jelsz)
+        if(idx ==-1){
+            alert('nincs ilyen user')
+            return
+        }
+        alert('sikeres belépés')
+
+/*     if(idx==-1){
+        alert('nincs ilyen user')
+        }
+        else{
+            //alert('megvan a user')
+            if(jelsz == userek[idx].password)
+            {
+                alert('sikeres belépés')
+        }else 
+            {
+                alert('Hibás jelszó')
+            }
+            }
+*/          
+}
